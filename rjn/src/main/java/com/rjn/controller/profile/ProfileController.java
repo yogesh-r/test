a156a1efd618de52ed3f94b0d38c99b3afa3070c
arnab92 <arnab.layek1992@gmail.com>
2017-01-29 15:34:00 +0530

    jhh

--- rjn/src/main/java/com/rjn/controller/profile/ProfileController.java
+++ rjn/src/main/java/com/rjn/controller/profile/ProfileController.java
@@ -7,13 +7,17 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.rjn.model.VendorProfile;
+import com.rjn.model.Branch.BranchMasterDetails;
+import com.rjn.service.BranchService;
 import com.rjn.service.VendorService;
 import com.rjn.service.Core.ApplicationUtils;
+import com.rjn.service.impl.BranchServiceImpl;
 
 @Controller
 @RequestMapping("/vendor-profile")
@@ -24,6 +28,9 @@ public class ProfileController {
 	
 	@Autowired
 	VendorService partnerService; 
+	
+	@Autowired
+	BranchService branch_service;
 
 	@RequestMapping(value = { "/{partId}" }, method = RequestMethod.GET)
 	public String paernerHome(ModelMap model, HttpServletRequest request, @PathVariable String partId) {
@@ -35,5 +42,19 @@ public class ProfileController {
 			model.put("headerType", loginUser.get(0));
 		}
 		return "vendor-profile";
+	}
+	
+	@RequestMapping(value = { "/{partId}/branch-list" }, method = RequestMethod.GET)
+	public String branchList(HttpServletRequest request,ModelMap model,@PathVariable String partId) {
+		Object object =  request.getSession().getAttribute("authorities");
+		List loginUser  = (List)object;
+		if (loginUser != null) {
+			model.put("headerType", loginUser.get(0));
+		}
+		List<BranchMasterDetails> branch_details=branch_service.getBranchByPartner(partId);
+		System.out.println("branch details?>>> "+branch_details);
+		model.addAttribute("branch",branch_details);
+		return "vendor-profile";
+		
 	}
 }
