a8a1a73830ff779ef48d5bfd15e5342b0e3cfb3b
arnab92 <arnab.layek1992@gmail.com>
2017-01-29 15:33:06 +0530

    testtttt

---
+++ rjn/src/main/java/com/rjn/controller/ForgetPasswordController.java
@@ -0,0 +1,58 @@
+package com.rjn.controller;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.rjn.model.ProfileMaster;
+import com.rjn.model.core.Email;
+import com.rjn.service.HeaderService;
+import com.rjn.service.Core.MailService;
+
+@Controller
+public class ForgetPasswordController {
+	
+	@Autowired
+	HeaderService headerService;
+	
+	@Autowired 
+	MailService mailService;
+	
+	@RequestMapping(value = { "/forgetPassword" }, method = RequestMethod.POST)
+	public String forgetPassword(ModelMap model, @RequestParam("email") String contactEmailId, HttpServletRequest request) {
+		ProfileMaster profileMaster = headerService.getProfileMasterByEmail(contactEmailId);
+		if (profileMaster == null) {
+			throw new UsernameNotFoundException(contactEmailId);
+		} else {
+			// generating token
+			String token = UUID.randomUUID().toString();
+			// saving token in database
+			headerService.saveToken(profileMaster, token);
+			// generating forgotpassword link
+			String forgotPasswordLink = request.getContextPath() + "/user/changePassword?id=" + 
+					profileMaster.getProfileNumber() + "&token=" + token;
+			// sending email
+			Email thisEmail = new Email();
+			thisEmail.setTo(contactEmailId);
+	    	thisEmail.setSubject("Test subject");
+	    	thisEmail.setBody("Forgot password link = "+forgotPasswordLink);
+	    	boolean isEmailSent = mailService.sendEmail(thisEmail);
+	    	System.out.println("isEmailSent >>> "+isEmailSent);
+		}
+		return null;
+	}
+	
+	@RequestMapping(value = { "/forgetPassword/link" }, method = RequestMethod.POST)
+	public String forgetPasswordLink(ModelMap model, @RequestParam("email") String contactEmailId, HttpServletRequest request) {
+		return null;
+	}
+
+}
