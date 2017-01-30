dc28f500a435d0fd07354ad5f5e429e647fe2034
arnab92 <arnab.layek1992@gmail.com>
2017-01-30 22:46:50 +0530

    Arnab

---
+++ rjn/src/main/java/com/rjn/dao/ProductDetailsDao.java
@@ -0,0 +1,10 @@
+package com.rjn.dao;
+
+import java.util.List;
+
+import com.rjn.model.core.ProductCategory;
+
+public interface ProductDetailsDao {
+	List<ProductCategory> getProductDetails();
+
+}
---
+++ rjn/src/main/java/com/rjn/service/ProductDetailsService.java
@@ -0,0 +1,10 @@
+package com.rjn.service;
+
+import java.util.List;
+
+import com.rjn.model.core.ProductCategory;
+
+public interface ProductDetailsService {
+	List<ProductCategory> getProductDetails();
+
+}
